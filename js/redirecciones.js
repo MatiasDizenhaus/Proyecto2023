361ac9aff66e6a549736a716d80ae4e4c2da1172
aribialo11 <127206610+aribialo11@users.noreply.github.com>
2023-10-23 14:30:06 -0300

    creo que termine todas las paginas

--- js/redirecciones.js
+++ js/redirecciones.js
@@ -57,7 +57,7 @@ $("#registrate").click(function(){
     window.location.replace("registro_donante.html");
 });
 
-$("#posibles_candidatos").click(function(){
+$("#candidatos").click(function(){
     window.location.replace("candidatos.html");
 });
 
@@ -67,6 +67,26 @@ $("#compatibilidad").click(function(){
 
 $("#registro_clinica").click(function(){
     window.location.replace("registro_clinica.html");
+});
+
+$("#registro_medico").click(function(){
+    window.location.replace("registro_medico.html");
+});
+
+$("#donante_registrado").click(function(){
+    window.location.replace("donanteregistrado.html");
+});
+
+$("#buscador").click(function(){
+    window.location.replace("buscador.html");
+});
+
+$("#clinica_registrada").click(function(){
+    window.location.replace("clinica_registrada.html");
+});
+
+$("#medico_registrado").click(function(){
+    window.location.replace("medicoregistrado.html");
 });
 
 });
